fe497bc944ab991ef63bca25c53fe2f6316006fc
Chris Myers <cmyers78@gmail.com>
2022-08-08 10:32:25 -0400

    initial commit

---
+++ ChapterTwo-ASK/ChapterTwo-ASK/ChapterTwo_ASKApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChapterTwo_ASKApp.swift
+//  ChapterTwo-ASK
+//
+//  Created by Chris Myers on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ChapterTwo_ASKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
