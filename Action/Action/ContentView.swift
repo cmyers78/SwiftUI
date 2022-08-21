1f4263926a7fec7df0c429be71275c5daf2e9523
Chris Myers <cmyers78@gmail.com>
2022-08-21 14:04:16 -0400

    adding slider to move action arrows around screen

--- Action/Action/ContentView.swift
+++ Action/Action/ContentView.swift
@@ -7,8 +7,13 @@
 
 import SwiftUI
 
+//private let colors: [Color] = [.red, .mint, .purple]
+
 struct ContentView {
     @State private var currentValue: Int = 0
+    @State private var horizontalSpace = 0.0
+//    @State private var inpuText = ""
+//    @State private var selectedIndex = 0
     
 }
 
@@ -16,10 +21,23 @@ extension ContentView: View {
     var body: some View {
         VStack (spacing: 30) {
             IntDisplay(value: currentValue)
-            HStack (spacing: 20) {
+            HStack (spacing: horizontalSpace) {
                 SymbolButton("arrow.left", action: back)
                 SymbolButton("arrow.right", action: forward)
             }
+ //           .foregroundColor(colors[selectedIndex])
+            Text(horizontalSpace.description)
+            Slider(value: $horizontalSpace, in: 0...100)
+                .padding(.horizontal)
+//            TextField("Enter Text", text: $inpuText)
+//                .multilineTextAlignment(.center)
+//            Picker("Font Style", selection: $selectedIndex) {
+//                ForEach(colors.indices) { index in
+//                    Text(colors[index].description.capitalized)
+//                }
+//            }
+//            .pickerStyle(.segmented)
+//            .padding(.horizontal)
    //         .buttonStyle(DoubleBorderButtonStyle())
         }
     }
